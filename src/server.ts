f83946012d2cf2544fb6aff63cd3cba6e80cc6a2
WelissonLuca <welissonluca17@gmail.com>
2021-06-21 14:06:12 -0300

    feat: add midlleware erros use

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,26 @@
-import 'reflect-metadata';
-import express from 'express';
+import "reflect-metadata";
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
 
-import "./database"
+import "./database";
+import { AppError } from "./error/AppError";
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  return res.send('ola')
-})
+app.get("/", (req, res) => {
+	return res.send("ola");
+});
 
-app.listen( 3333, () => console.log('server is running'))
+app.use(
+	(err: Error, request: Request, response: Response, next: NextFunction) => {
+		if (err instanceof AppError)
+			return response.status(err.statusCode).json({ message: err.message });
+		return response.status(500).json({
+			status: "error",
+			message: `Internal server error - ${err.message}`,
+		});
+	}
+);
+
+app.listen(3333, () => console.log("server is running"));
