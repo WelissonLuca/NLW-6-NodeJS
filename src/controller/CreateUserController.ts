57fd326e3a33124059c7d51b4a01a8f213e05e70
WelissonLuca <welissonluca17@gmail.com>
2021-06-21 17:47:48 -0300

    feat: add create user controller method handle

---
+++ src/controller/CreateUserController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express";
+import { CreateUserService } from "../services/CreateUserService";
+class CreateUserController {
+	async handle(request: Request, response: Response) {
+		const { name, email, admin } = request.body;
+
+		const createUserService = new CreateUserService();
+
+		const user = createUserService.execute({ name, email, admin });
+
+		return response.status(201).json(user);
+	}
+}
+
+
+export { CreateUserController };
