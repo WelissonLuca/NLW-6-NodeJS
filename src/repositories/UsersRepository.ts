b3f5b0709378db27faf8d79fa5dcfbd0842a8dbf
WelissonLuca <welissonluca17@gmail.com>
2021-06-22 08:11:49 -0300

    refactor: project architeture SOLID clean

--- src/repositories/UsersRepository.ts
+++
@@ -1,9 +0,0 @@
-import { EntityRepository, Repository } from "typeorm";
-import { User } from "../entity/User";
-
- 
-@EntityRepository(User)
-class UsersRepositories extends Repository<User> {}
-
-
-export { UsersRepositories };
--- src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { UserRouter } from "./user.routes";
-
-const router = Router();
-
-router.use('/', UserRouter)
-
-export { router }
