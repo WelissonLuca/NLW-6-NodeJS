13021f6a30a3e50b981d611b653690cae8a3f488
WelissonLuca <welissonluca17@gmail.com>
2021-06-23 11:05:13 -0300

    feat: register singleton tag repository

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,6 +1,9 @@
 import { container } from 'tsyringe';
 
+import { TagRepository } from '@modules/accounts/infra/typeorm/repositories/TagRepository';
 import { UserRepository } from '@modules/accounts/infra/typeorm/repositories/UserRepository';
+import { ITagRepository } from '@modules/accounts/repositories/ITagRepository';
 import { IUserRepository } from '@modules/accounts/repositories/IUsersRepository';
 
 container.registerSingleton<IUserRepository>('UserRepository', UserRepository);
+container.registerSingleton<ITagRepository>('TagRepository', TagRepository);
