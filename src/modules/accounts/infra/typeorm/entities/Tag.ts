4f7dbd5a0bfeca641b87a4263afdb3f05f50e672
WelissonLuca <welissonluca17@gmail.com>
2021-06-23 00:13:12 -0300

    feat: infra repositories add tag repository

---
+++ src/modules/accounts/infra/typeorm/entities/Tag.ts
@@ -0,0 +1,29 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { v4 as uuid } from 'uuid';
+
+@Entity('tags')
+class Tag {
+  @PrimaryColumn()
+  readonly id: string;
+
+  @Column()
+  name: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  constructor() {
+    this.id = this.id ?? uuid();
+  }
+}
+
+export { Tag };
