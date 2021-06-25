928706831e31f5479c8be53cfbc4b6e760139aa8
WelissonLuca <welissonluca17@gmail.com>
2021-06-25 00:43:28 -0300

    feat: add all routes list

--- src/shared/infra/http/routes/tags.routes.ts
+++ src/shared/infra/http/routes/tags.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import { CreateTagController } from '@modules/accounts/useCases/createTags/CreateTagController';
+import { ListTagsController } from '@modules/accounts/useCases/listTags/ListTagController';
 
 import { ensureAdmin } from '../middlewares/ensureAdmin';
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
@@ -8,12 +9,19 @@ import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 const tagRouter = Router();
 
 const createTagController = new CreateTagController();
-
+const listTagsController = new ListTagsController();
 tagRouter.post(
   '/tags',
   ensureAuthenticated,
   ensureAdmin,
   createTagController.handle
+);
+
+tagRouter.get(
+  '/tags',
+  ensureAuthenticated,
+  ensureAdmin,
+  listTagsController.handle
 );
 
 export { tagRouter };
